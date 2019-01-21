3613bd8499a167844846a7f8ae53e4b300137bcb
araujohelder <heldersilvaaraujo@gmail.com>
2019-01-21 18:54:39 -0200

    removendo as informações do firebase

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCHrdk072DSmiJ6X2_mvxvzt7Xkcwri-2c",
-    authDomain: "fir-login-7a316.firebaseapp.com",
-    databaseURL: "https://fir-login-7a316.firebaseio.com",
-    projectId: "fir-login-7a316",
-    storageBucket: "fir-login-7a316.appspot.com",
-    messagingSenderId: "266156090175"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
